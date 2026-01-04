db68fb5244645bd51b39e277e1b5376c6aa709b1
Naveen khatri <naveen1khatri@gmail.com>
2026-01-04 12:43:39 +0530

    Delete sorry.js

--- sorry.js
+++
@@ -1,7 +0,0 @@
-const sorryBtn = document.getElementById("sorryBtn");
-const response = document.getElementById("response");
-
-sorryBtn.addEventListener("click", () => {
-  response.classList.remove("hidden");
-  response.innerHTML = "💖 Tumhe khona nahi chahta baby 💖";
-});
